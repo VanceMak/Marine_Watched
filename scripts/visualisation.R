1df3bca003037d8ef8dc7e374097cc69af0963d2
vancemak <vancemak@Vances-MacBook-Pro.local>
2023-10-23 16:38:13 +0100

    Added gradients to line and points

--- scripts/visualisation.R
+++ scripts/visualisation.R
@@ -4,19 +4,20 @@
 library(pacman)
 pacman:: p_load(here, dplyr, tidyverse, sf, ggplot2, gganimate, gifski, terra, tidyterra)
 
-
 #read data <===== change acccording to visualisation 
   #GPS TRACK OF BOAT TRIP
 trip <- read.csv(here::here("raw_data","2023_09_22","waypoints.csv"),row.names=NULL) %>%
   st_as_sf(coords = c("X", "Y"), crs=4326, remove = FALSE) %>%
-  summarize(do_union = FALSE) %>%
-  st_cast("LINESTRING") 
+  mutate(order = row_number()) #%>%
+  #summarize(do_union = FALSE) %>%
+  #st_cast("LINESTRING") 
 
   #GPS POINTS OF SIGHTINGS
 sightings <- read.csv(here::here("raw_data","2023_09_22","points.csv"),row.names=NULL) %>%
   filter(!is.na(Latitude)) %>% 
   filter(!is.na(Longitude)) %>%
-  st_as_sf(coords = c("Latitude", "Longitude"), crs=4326, remove = FALSE)
+  st_as_sf(coords = c("Latitude", "Longitude"), crs=4326, remove = FALSE) %>%
+  mutate(species = as.factor(Group))
 
 
 #read spatial files <=== do not change
@@ -30,19 +31,24 @@ bathy2 = rast(here::here("spatial_files","bathy","mtf5050010055.asc"))
 
 #plot <=== do not change
 plot <- ggplot() + 
+  theme(panel.background = element_blank(), panel.ontop = TRUE,
+        panel.grid.major = element_line(linetype = "solid", 
+                                        colour = "grey30", linewidth =0.075),
+        panel.border = element_rect(colour="transparent", fill = "transparent"),
+        legend.position = "none") +
   geom_spatraster(data=bathy) +
   geom_spatraster(data=bathy2) +
   scale_fill_gradient(high = "#cedce3", low = "#0073ad",
-                      breaks = c(0, -10, -50, -60),guide="none") +
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_line(linetype = "solid", colour = "grey30",size=0.075),
-        panel.ontop = TRUE,
-        panel.border = element_rect(colour="transparent", fill = "transparent")) +
+                      breaks = c(0, -10, -50, -60)) +
   geom_sf(data=falbay, colour = "#cedce3", fill = "grey75",lwd=0.5) +  
   geom_sf(data=falmouth, colour = "grey60", fill = "grey60") + 
-  geom_sf(data=trip, size = 0.2, colour="red",alpha = 0.5) +
-  geom_point(data=sightings, size = 3, aes (x = Longitude, y = Latitude,
-                                            colour = Group)) +
+  geom_path(data=trip, linewidth = 0.75, aes(x = X, y = Y, colour = order)) + 
+  scale_colour_gradient2(high = "darkgreen", mid = "white", low = "darkgoldenrod1", 
+                         midpoint = max(trip$order)/2) +
+  ggnewscale::new_scale_colour() +  
+  geom_point(data=sightings, aes(x = Longitude, y = Latitude, colour = species), 
+             size = 3,alpha = 0.8) +
+  ggnewscale::new_scale_colour() + 
   coord_sf(xlim = c(-5.15,-4.75), ylim = c(50.01, 50.2)) +
   scale_y_continuous(breaks = seq(50.07, 50.19, by = 0.1)) +
   NULL
